73dfe192f479ef834c81c0b56224ea111e4ac0b8
sorci89 <sore.eszter@gmail.com>
2021-10-26 15:04:30 +0200

    10.26. during class

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
+function fastLog(text){
+    console.log(text);
+    return "text logged muhaha";
+}
+
+function buttonClicked(event){
+// console.log(event.target);
+
+ // console.log(event.target.classList);
+
+    event.target.classList.toggle("clicked");
+
+    const fastLogResult = fastLog("Ezt a szöveget szeretném kiloggoltatni");
+    console.log(fastLogResult);
+}
+
 function loadEvent() {
     console.log('the page has loaded');
 
-    document.getElementById("menu-btn").addEventListener("click",function (event){
-        console.log(event.target)
-        event.target.classList.toggle("clicked");
-    });
+    document.getElementById("menu-btn").addEventListener("click", buttonClicked);
 }
 
 window.addEventListener("load", loadEvent);
