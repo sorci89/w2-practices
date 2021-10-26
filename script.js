2e670f0befdbfc6275c6bc04301e1295d973d984
sorci89 <sore.eszter@gmail.com>
2021-10-26 15:45:14 +0200

    kék nav bar, piros-fekete button, clicked/nem clicked

--- script.js
+++ script.js
@@ -1,18 +1,8 @@
-function fastLog(text){
-    console.log(text);
-    return "text logged muhaha";
-}
-
 function buttonClicked(event){
-// console.log(event.target);
-
- // console.log(event.target.classList);
-
     event.target.classList.toggle("clicked");
-
-    const fastLogResult = fastLog("Ezt a szöveget szeretném kiloggoltatni");
-    console.log(fastLogResult);
-}
+    
+    document.getElementById("nav").classList.toggle("clicked");
+}    
 
 function loadEvent() {
     console.log('the page has loaded');
