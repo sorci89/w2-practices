52da65e1b7e3d34bd3253e1821fac9bb77368166
sorci89 <sore.eszter@gmail.com>
2021-10-27 12:39:49 +0200

    HW: body gets class clicked, nav bar and button still changing

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function buttonClicked(event){
     event.target.classList.toggle("clicked");
     
-    document.getElementById("nav").classList.toggle("clicked");
+   // document.getElementById("nav").classList.toggle("clicked");
 }    
 
 function loadEvent() {
@@ -12,8 +12,7 @@ function loadEvent() {
 
 window.addEventListener("load", loadEvent);
 
-/*
 function buttonClicked(event){
    document.getElementById("bodyId").classList.toggle("clicked");
-} */   
+} 
 
